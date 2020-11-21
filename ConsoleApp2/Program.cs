34bda300369a059cc1d694e77a0ce936ed6e4535
Rustam Sayfutdinov <rstm.sf@gmail.com>
2020-11-21 06:48:37 +0300

    Play with auto-download

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine($"Environment.Version: {Environment.Version}");
+        }
+    }
+}
